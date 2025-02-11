56a803794d9fe01e7308a6953e055dbda8cae889
maneesha herath <72514560+Maneeshaprabha@users.noreply.github.com>
2025-02-11 20:46:46 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['https://www.playmade.co.uk/'],
+    origin: ['https://maneeshaprabha.github.io'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
